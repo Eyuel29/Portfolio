8ef85a5fed1336e2ce244fbc5687d09fc2f47964
eyuel <dejenejoel@gmail.com>
2024-01-24 20:12:05 +0300

    bugs and fixes

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,12 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import "../../assets/css/Base.css";
+import "../../assets/css/About.css";
 
 const About = () =>{
 
     return (
-        <div className="projects">
-            <h1 className="card">About page</h1>
+        <div className="about">
+            <div>
+                <div className='about-img'>
+                    
+                    <img src='/media/personal-nobg.png' alt='me'/>
+                    <div>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Quidem itaque, magnam ratione assumenda recusandae reprehenderit 
+                        illo hic ea praesentium adipisci.
+                        </p>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 
